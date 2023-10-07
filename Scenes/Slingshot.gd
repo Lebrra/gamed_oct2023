3191f8dfb8b73e9c80931bc25b80de458cbb762b
BlasczykLeah <scout1665@gmail.com>
2023-10-07 17:15:04 -0500

    Player model imported

--- Scenes/Slingshot.gd
+++ Scenes/Slingshot.gd
@@ -5,8 +5,14 @@ var fired = false
 @export var power: float = 5
 @export var powerBuildUpSpeed = 25
 @export var maxPower = 100
+@export var anim: AnimationPlayer
 
 func _physics_process(delta):
+	if linear_velocity.y < 0.0001 && linear_velocity.y > -0.0001:
+		anim.play("Idle_001")
+		print("Idle")
+	else:
+		anim.play("Flying")
 	if Input.is_action_pressed("Fire"):
 		power += delta * powerBuildUpSpeed
 	if Input.is_action_just_released("Fire"):
@@ -14,4 +20,5 @@ func _physics_process(delta):
 		var direction = position.direction_to(Arrow.global_position)
 		linear_velocity = power * direction
 		power = 0
+		anim.play("Flying")
 	UiGlobal.UpdatePowerUI.emit(power)
