772dcdfb07f6167b90f97fed55d88d2dafcabfa4
MrAndrewRobertMason <mrandrewrobertmason@gmail.com>
2023-10-08 00:34:53 -0500

    UNDER MY UMBER ELLA ELLA ELLA EY EY EY

--- Scripts/Pole.cs
+++ Scripts/Pole.cs
@@ -3,27 +3,69 @@ using System;
 
 public partial class Pole : Node3D
 {
+	[Export] RigidBody3D rb;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	[Export] Node3D umbrellaClosed;
+	[Export] Node3D umbrellaOpen;
+
+	Camera3D cam;
+	Vector2 mousePos;
+	Plane plane;
+
+	Vector3 directionTo;
+
+	public override void _Ready() {
+		cam = GetViewport().GetCamera3D();
+		plane = new Plane(Vector3.Back, Vector3.Zero);
+	}
+
+	public override void _PhysicsProcess(double delta){
 		LookAtCursor();
+		DragQueen(delta);
 	}
 	
-	void LookAtCursor()
-	{
-		var cam = GetViewport().GetCamera3D();
-		var mousePos = GetViewport().GetMousePosition();
+	void LookAtCursor(){
+		mousePos = GetViewport().GetMousePosition();
 		var from = cam.ProjectRayOrigin(mousePos);
-		var to = from + cam.ProjectRayNormal(mousePos) * 1000;
-		var cursor = new Plane(Vector3.Back, GlobalTransform.Origin.Y).IntersectsRay(from, to);
+		var to = cam.ProjectRayNormal(mousePos) * 1000;
+		var cursor = plane.IntersectsRay(from, to);
 
-		if (cursor != null)
-		{
-			var dist = GlobalPosition.DirectionTo(cursor.Value);
-			var angle = Mathf.Atan2(dist.Y, dist.X);
+		if (cursor != null){
+			directionTo = GlobalPosition.DirectionTo(cursor.Value);
+			var angle = Mathf.Atan2(directionTo.Y, directionTo.X);
 			Rotation = new Vector3(0, 0, angle);
+
+		}
+
+	}
+
+	void DragQueen(double delta) {
+		Vector3 velocity = rb.LinearVelocity;
+		float dragCoefficient;
+
+		if (velocity.AngleTo(directionTo) > Mathf.Pi/2f){
+			dragCoefficient = 1f;
+			if (rb.LinearVelocity.LengthSquared() > 0.025f) {
+				ToggleUmbrellaModel(true);
+			}
 			
+		}
+		else {
+			dragCoefficient = -.25f;
+			ToggleUmbrellaModel(false);
+		}
+
+		rb.LinearVelocity *= (1 - dragCoefficient * (float)delta);
+	}
+
+	void ToggleUmbrellaModel(bool open) {
+		if (open){
+			if (umbrellaClosed != null) { umbrellaClosed.Hide(); }
+			if (umbrellaOpen != null) { umbrellaOpen.Show(); }
+		}
+		else {
+			if (umbrellaClosed != null) { umbrellaClosed.Show(); }
+			if (umbrellaOpen != null) { umbrellaOpen.Hide(); }
 		}
 	}
 
