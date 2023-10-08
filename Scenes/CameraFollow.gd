9672163f90e69b732c24c2e316bb4191ceea3ca7
BlasczykLeah <scout1665@gmail.com>
2023-10-07 22:36:51 -0500

    Jerk Off

--- Scenes/CameraFollow.gd
+++ Scenes/CameraFollow.gd
@@ -10,7 +10,7 @@ func _ready():
 	playerNode = get_node(player)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	var v = playerNode.linear_velocity
 	v.x = clampf(v.x, -1, 1)
 	v.y = clampf(v.y, -1, 1)
